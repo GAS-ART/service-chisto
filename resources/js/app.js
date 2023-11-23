03f3357ae9ea1316a02b0c5de6a43abe231cb46c
GAS-ART <temoha1386@gmail.com>
2023-11-23 11:22:37 +0200

    Fixed select visability

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,99 +2,111 @@ import './bootstrap';
 import {popUp} from './modules/popup.js';
 import './modules/AsyncFormSending.js';
 
-const HTMLBody = document.querySelector('body')
+window.onload = function () {
 
-const burgerIcon = document.querySelector('.burger_icon');
-const burger = document.querySelector('.burger');
+    const HTMLBody = document.querySelector('body')
 
-if(burgerIcon && burger){
+    const burgerIcon = document.querySelector('.burger_icon');
+    const burger = document.querySelector('.burger');
 
-  burgerIcon.addEventListener('click', () => {
-    burgerIcon.classList.toggle('active');
-    burger.classList.toggle('active');
-    HTMLBody.classList.toggle('lock');
-  });
-}
+    if (burgerIcon && burger) {
+
+        burgerIcon.addEventListener('click', () => {
+            burgerIcon.classList.toggle('active');
+            burger.classList.toggle('active');
+            HTMLBody.classList.toggle('lock');
+        });
+    }
 
 //popup
-const popupLinks = document.querySelectorAll('.link-on-popup');
-popupLinks.forEach(popupLink => {
-  popupLink.addEventListener('click', () => popUp(popupLink.dataset.popup));
-});
+    const popupLinks = document.querySelectorAll('.link-on-popup');
+    popupLinks.forEach(popupLink => {
+        popupLink.addEventListener('click', () => popUp(popupLink.dataset.popup));
+    });
 
-const topMenuBtn = document.querySelector('.top-menu-btn');
-const headerBottomMenu = document.querySelector('.header__bottom-menu');
-const header = document.querySelector('.header');
-if(topMenuBtn && headerBottomMenu){
-  topMenuBtn.addEventListener('click', () => {
-    headerBottomMenu.classList.add('active');
-    header.classList.add('save-top')
-  });
-  document.addEventListener('scroll', (e)=> {
-    if (window.scrollY > 100) {
-      topMenuBtn.classList.add('active');
-    } else {
-      topMenuBtn.classList.remove('active');
+    const topMenuBtn = document.querySelector('.top-menu-btn');
+    const headerBottomMenu = document.querySelector('.header__bottom-menu');
+    const header = document.querySelector('.header');
+    if (topMenuBtn && headerBottomMenu) {
+        topMenuBtn.addEventListener('click', () => {
+            headerBottomMenu.classList.add('active');
+            header.classList.add('save-top')
+        });
+        document.addEventListener('scroll', (e) => {
+            if (window.scrollY > 100) {
+                topMenuBtn.classList.add('active');
+            } else {
+                topMenuBtn.classList.remove('active');
+            }
+            if (headerBottomMenu.classList.contains('active')) {
+                headerBottomMenu.classList.add('hide');
+                setTimeout(() => {
+                    headerBottomMenu.classList.remove('hide');
+                    headerBottomMenu.classList.remove('active');
+                    header.classList.remove('save-top');
+                }, 300);
+            }
+        });
     }
-    if(headerBottomMenu.classList.contains('active')){
-      headerBottomMenu.classList.add('hide');
-      setTimeout(()=> {
-        headerBottomMenu.classList.remove('hide');
-        headerBottomMenu.classList.remove('active');
-        header.classList.remove('save-top');
-      }, 300);
-    }
-  });
-}
 
 
 //Set CSS variables
 
-function svgVariables() {
-  /* const polisherWidth = document.querySelector('.polisher svg').clientWidth; */
-  const styleTag = document.querySelector("head style");
-  const cleanerImageheigt = document.querySelector('.brush').clientHeight
-  const sofaImageWidts = document.querySelector('.sofa').clientWidth
-  //--polisher-width: ${polisherWidth}px;
-  styleTag.innerHTML = `:root {--cleaner-icon-height: ${cleanerImageheigt}px; --sofa-width: ${sofaImageWidts}px;}`; 
-}
+    function svgVariables() {
+        /* const polisherWidth = document.querySelector('.polisher svg').clientWidth; */
+        const styleTag = document.querySelector("head style");
+        const cleanerImageheigt = document.querySelector('.brush').clientHeight
+        const sofaImageWidts = document.querySelector('.sofa').clientWidth
+        //--polisher-width: ${polisherWidth}px;
+        styleTag.innerHTML = `:root {--cleaner-icon-height: ${cleanerImageheigt}px; --sofa-width: ${sofaImageWidts}px;}`;
+    }
 
-svgVariables();
+    svgVariables();
 
-window.addEventListener('resize', svgVariables);
+    window.addEventListener('resize', svgVariables);
 
-/* const styleTag = document.querySelector("head style");
-styleTag.innerHTML += `:root { --scrollbar-width: ${scrollbarWidth}px; }`; */
+    /* const styleTag = document.querySelector("head style");
+    styleTag.innerHTML += `:root { --scrollbar-width: ${scrollbarWidth}px; }`; */
 
 //Animation
-document.querySelector('.window').classList.add('active');
-document.querySelector('.mop').classList.add('active');
-document.querySelector('.vacun-cleaner').classList.add('active');
-//document.querySelector('.garbage').classList.add('active');
-//document.querySelector('.brush').classList.add('active')
-setTimeout(()=> document.querySelector('.garbage').classList.add('active'), 5800);
-setTimeout(()=> document.querySelector('.garbage').classList.add('visible'), 7200);
-setTimeout(()=> document.querySelector('.brush').classList.add('active'), 6400);
-setTimeout(()=> document.querySelector('.brush').classList.add('visible'), 7200);
-setTimeout(()=> document.querySelector('.sofa').classList.add('active'), 7500);
-setTimeout(()=> document.querySelector('.sofa').classList.add('visible'), 9000);
+    document.querySelector('.window').classList.add('active');
+    document.querySelector('.mop').classList.add('active');
+    document.querySelector('.vacun-cleaner').classList.add('active');
+    setTimeout(() => document.querySelector('.garbage').classList.add('active'), 5800);
+    setTimeout(() => document.querySelector('.garbage').classList.add('visible'), 7200);
+    setTimeout(() => document.querySelector('.brush').classList.add('active'), 6400);
+    setTimeout(() => document.querySelector('.brush').classList.add('visible'), 7200);
+    setTimeout(() => document.querySelector('.sofa').classList.add('active'), 7500);
+    setTimeout(() => document.querySelector('.sofa').classList.add('visible'), 9000);
 
 
 //Showe-more-mobile
-const showMoreBtn = document.querySelectorAll('.show-more-btn');
+    const showMoreBtn = document.querySelectorAll('.show-more-btn');
 
-showMoreBtn.forEach(btn =>{
-  setTimeout(()=>{
-    const animatedEl = btn.previousElementSibling
-    const height = animatedEl.scrollHeight;
-    btn.addEventListener('click', ()=>{
-      animatedEl.style.height = height+'px';
-      btn.remove();
-      btn.classList.add('inactive');
-      setTimeout(()=>{
-        animatedEl.style.height = 'auto';
-      },1000)
+    showMoreBtn.forEach(btn => {
+            const animatedEl = btn.previousElementSibling
+            const height = animatedEl.scrollHeight;
+            btn.addEventListener('click', () => {
+                animatedEl.style.height = height + 'px';
+                btn.remove();
+                btn.classList.add('inactive');
+                setTimeout(() => {
+                    animatedEl.style.height = 'auto';
+                }, 1000)
+            });
     });
-  }, 500)
-} );
 
+//Changed Select Color
+  const selects = document.querySelectorAll('select');
+
+  function changeSelectHandler() {
+    this.style.color = '#000000';
+    this.removeEventListener('change', changeSelectHandler);
+  }
+
+  selects.forEach(select => {
+    select.addEventListener('change', changeSelectHandler);
+  });
+
+  //End onloud function
+}
--- resources/views/components/lead.blade.php
+++ resources/views/components/lead.blade.php
@@ -4,20 +4,19 @@
             <div class="lead-form__contact">
                 <h2 class="lead-form__title">@lang('lead_form.title')</h2>
                 <p class="lead-form__text">@lang('lead_form.text')</p>
-                <form class="lead-form__form"  action="{{ route('sendMainForm') }}" method="POST" name="mainForm" id="mainForm"
-                      class="@lang('lang.current')">
+                <form class="lead-form__form" action="{{ route('sendMainForm') }}" method="POST" name="mainForm"
+                    id="mainForm" class="@lang('lang.current')">
                     <div class="lead-form__loading">
                         <img src="{{ asset('img/load.gif') }}" alt="">
                     </div>
                     @csrf
-                    <input name="page" type="hidden" value="{{url()->current()}}">
+                    <input name="page" type="hidden" value="{{ url()->current() }}">
                     <div class="lead-form__form-item">
                         <label for="name">@lang('popup.name')</label>
-                        <input id="name" name="name" type="text"
-                               placeholder="@lang('popup.name_placeholder')">
+                        <input id="name" name="name" type="text" placeholder="@lang('popup.name_placeholder')">
                         <p class="error-name"></p>
                     </div>
-                    <div class="lead-form__form-item">
+                    <div class="lead-form__form-item select">
                         <label for="form-select">@lang('common.select.popup')</label>
                         <select name="service" id="form-select">
                             <option hidden disabled selected value="">@lang('common.select.placeholder')</option>
@@ -32,15 +31,14 @@
                     </div>
                     <div class="lead-form__form-item">
                         <label for="phone">@lang('popup.phone')</label>
-                        <input id="phone" name="phone" type="text"
-                               placeholder="@lang('popup.phone_placeholder')">
+                        <input id="phone" name="phone" type="text" placeholder="@lang('popup.phone_placeholder')">
                         <p class="error-phone"></p>
                     </div>
                     <button class="lead-form__btn" type="submit">@lang('lead_form.btn')</button>
                 </form>
             </div>
             <div class="lead-form__img">
-                <img src="{{asset('img/lead_form/1.webp')}}" alt="">
+                <img src="{{ asset('img/lead_form/1.webp') }}" alt="">
             </div>
         </div>
     </div>
