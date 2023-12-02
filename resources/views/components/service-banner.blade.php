893b79f22811582122c9bb3b849d4de53ccf638c
GAS-ART <temoha1386@gmail.com>
2023-12-02 08:22:41 +0200

    Added clock image

--- resources/views/components/service-banner.blade.php
+++ resources/views/components/service-banner.blade.php
@@ -1,6 +1,6 @@
 <div class="service-banner">
     <div class="service-banner__img">
-        <{{-- source type="image/webp" srcset="{{ asset($banner['img']) }}" media="(min-width: 768px)">
+        {{-- source type="image/webp" srcset="{{ asset($banner['img']) }}" media="(min-width: 768px)">
         <source type="image/webp" srcset="{{ asset($banner['img_mobile']) }}"> --}} <img src="{{ asset($banner['img']) }}" alt="">
     </div>
     <div class="service-banner__body">
