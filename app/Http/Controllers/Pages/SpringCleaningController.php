fa09f9b6abdfdc359ab2328c819baad9067c5aad
GAS-ART <temoha1386@gmail.com>
2023-12-20 17:05:03 +0200

    Added office and industrial pages

--- app/Http/Controllers/Pages/SpringCleaningController.php
+++ app/Http/Controllers/Pages/SpringCleaningController.php
@@ -162,10 +162,72 @@ class SpringCleaningController extends SetLangAndViewController
     }
 
     public function offices ($locale){
-        return $this->setLocaleAndView($locale, 'spring-cleaning.offices', []);
+
+        $banner = [
+            'img' => 'img/spring_cleaning/offices/banner.webp',
+            'title' => 'spring-cleaning.offices.title',
+        ];
+
+        $stages = [
+            'title' => 'spring-cleaning.offices.stages.title',
+            'text' => 'spring-cleaning.offices.stages.text',
+            'content' => [],
+        ];
+        for($i=1; $i<9; $i++){
+            $stages['content']["stage_$i"] = ['img' => "img/spring_cleaning/offices/stages/$i.webp",'title' => "spring-cleaning.offices.stages.stage_$i.title", 'text' =>"spring-cleaning.offices.stages.stage_$i.text"]; 
+        }
+
+        $recommend = ['title' => 'spring-cleaning.offices.recommend.title', 'content' => []];
+        for ($i=1; $i < 4; $i++) { 
+            $recommend['content']["item_$i"] = ['img' => "img/spring_cleaning/offices/recommend/$i.webp",'title' => "spring-cleaning.offices.recommend.item_$i.title", 'text' => "spring-cleaning.offices.recommend.item_$i.text"];
+        }
+
+        $fourAdvantages = ['spring-cleaning.offices.four_advantages.title'];
+        for ($i=1; $i < 5; $i++) { 
+            $fourAdvantages[] = "spring-cleaning.offices.four_advantages.$i";
+            $fourAdvantages[] = "spring-cleaning.offices.four_advantages.0$i";
+        }
+
+        $typeClients = ['title' => 'spring-cleaning.offices.clients.title', 'content' => []];
+        for ($i=1; $i < 4; $i++) { 
+            $typeClients['content']["client_$i"] = ['img' => "img/spring_cleaning/offices/clients/$i.webp",'title' => "spring-cleaning.offices.clients.client_$i.title", 'text' => "spring-cleaning.offices.clients.client_$i.text"];
+        }
+
+        return $this->setLocaleAndView($locale, 'spring-cleaning.offices', compact('banner', 'stages', 'recommend', 'fourAdvantages', 'typeClients'));
     }
 
     public function industrial ($locale){
-        return $this->setLocaleAndView($locale, 'spring-cleaning.industrial', []);
+
+        $banner = [
+            'img' => 'img/spring_cleaning/industrial/banner.webp',
+            'title' => 'spring-cleaning.industrial.title',
+        ];
+
+        $stages = [
+            'title' => 'spring-cleaning.industrial.stages.title',
+            'text' => 'spring-cleaning.industrial.stages.text',
+            'content' => [],
+        ];
+        for($i=1; $i<9; $i++){
+            $stages['content']["stage_$i"] = ['img' => "img/spring_cleaning/industrial/stages/$i.webp",'title' => "spring-cleaning.industrial.stages.stage_$i.title", 'text' =>"spring-cleaning.industrial.stages.stage_$i.text"]; 
+        }
+
+        $recommend = ['title' => 'spring-cleaning.industrial.recommend.title', 'content' => []];
+        for ($i=1; $i < 4; $i++) { 
+            $recommend['content']["item_$i"] = ['img' => "img/spring_cleaning/industrial/recommend/$i.webp",'title' => "spring-cleaning.industrial.recommend.item_$i.title", 'text' => "spring-cleaning.industrial.recommend.item_$i.text"];
+        }
+
+        $fourAdvantages = ['spring-cleaning.industrial.four_advantages.title'];
+        for ($i=1; $i < 5; $i++) { 
+            $fourAdvantages[] = "spring-cleaning.industrial.four_advantages.$i";
+            $fourAdvantages[] = "spring-cleaning.industrial.four_advantages.0$i";
+        }
+
+        $typeClients = ['title' => 'spring-cleaning.industrial.clients.title', 'content' => []];
+        for ($i=1; $i < 7; $i++) { 
+            $typeClients['content']["client_$i"] = ['img' => "img/spring_cleaning/industrial/clients/$i.webp",'title' => "spring-cleaning.industrial.clients.client_$i.title", 'text' => "spring-cleaning.industrial.clients.client_$i.text"];
+        }
+
+        return $this->setLocaleAndView($locale, 'spring-cleaning.industrial',  compact('banner', 'stages', 'recommend', 'fourAdvantages', 'typeClients'));
     }
 }
