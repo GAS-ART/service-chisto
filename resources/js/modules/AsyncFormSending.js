b2bc40a9acc3e7510aff30d0f000972df02ee3c9
GAS-ART <temoha1386@gmail.com>
2024-05-05 16:33:14 +0300

    try to gat ads function when responce.ok

--- resources/js/modules/AsyncFormSending.js
+++ resources/js/modules/AsyncFormSending.js
@@ -33,7 +33,7 @@ forms.forEach(form => {
       if (response.ok) {
         console.log(gtag_report_conversion)
         loading.classList.remove('active');
-        form.closest('.popup').classList.add('sent');
+        form.closest('.popup') ? form.closest.classList.add('sent') : null;
         nameError.innerText = '';
         nameError.classList.remove('error');
         nameError.previousElementSibling.classList.remove('error');
