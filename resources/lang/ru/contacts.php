1bad523357f071e91341b027ddd35346dfec9062
GAS-ART <temoha1386@gmail.com>
2024-08-03 21:44:06 +0300

    Fixed phone link bug

--- resources/lang/ru/contacts.php
+++ resources/lang/ru/contacts.php
@@ -2,7 +2,7 @@
 
 return [
    'phone' => '+38 (063) 664 26 36',
-   'phone_link' => '63 664 26 36',
+   'phone_link' => '636642636',
    'email' => 'dnepr.jc@gmail.com',
    'email_link' => 'dnepr.jc@gmail.com',
    'instagram' => 'https://www.instagram.com/klining.ua/',
