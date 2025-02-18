bfc247a03da0c90a7dcf7bce0b0f96a33de137c1
GAS-ART <temoha1386@gmail.com>
2025-02-18 22:56:43 +0200

    Changed phone number

--- resources/lang/ru/contacts.php
+++ resources/lang/ru/contacts.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-   'phone' => '+38 (095) 553 12 66',
-   'phone_link' => '955531266',
+   'phone' => '+38 (050) 487 77 74',
+   'phone_link' => '504877774',
    'email' => 'inegra7249725@gmail.com',
    'email_link' => 'inegra7249725@gmail.com',
    'instagram' => 'https://www.instagram.com/klining.ua/',
