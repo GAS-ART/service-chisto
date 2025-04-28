61e66a6987e0394aca9be4895eab8cc070af9e99
GAS-ART <temoha1386@gmail.com>
2025-04-28 21:49:36 +0300

    Added new gtag

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -22,32 +22,14 @@
         </style>
         @vite(['resources/scss/style.scss'])
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16519033064">
-        </script>
+       <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10998840814"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'AW-16519033064');
-        </script>
-
-        <!-- Event snippet for Контакт conversion page
-        In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
-        <script>
-        function gtag_report_conversion(url) {
-        var callback = function () {
-            if (typeof(url) != 'undefined') {
-            window.location = url;
-            }
-        };
-        gtag('event', 'conversion', {
-            'send_to': 'AW-16519033064/XwTvCJPCrKwZEOjh8cQ9',
-            'event_callback': callback
-        });
-        return false;
-        }
+        gtag('config', 'AW-10998840814');
         </script>
 
 
